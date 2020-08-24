f713efe5d58754e5b1f311d3a65281a0a9393a24
smoir <moise605@student.otago.ac.nz>
2020-08-24 17:18:06 +1200

    added admin website

---
+++ public/admin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> To do list app </title>
+	</head>
+    <body>
+        <div class="heading">
+        <h2>This is an app for to do list.</h2>
+        </div>
+
+        <form method = "POST" action="index.php">
+            <input type = "text" name = "task" class = "task_input">
+            <button type = "submit" class = "task_btn" name = "submit"> Add Task </button>
+        </form>
+	</body>
+</html>

4ae78e02e6484c8e7380798fc92feb506bb74953
Sean Moir <seanmoir99@gmail.com>
2020-08-30 04:54:56 +1200

    test data for DB

--- db-init.sql
+++ db-init.sql
@@ -7,3 +7,8 @@ CREATE TABLE todo_list (
     id int PRIMARY KEY AUTO_INCREMENT,
     task varchar(255) UNIQUE NOT NULL
 );
+
+INSERT INTO todo_list(task) VALUES ('test');
+INSERT INTO todo_list(task) VALUES ('abcd');
+INSERT INTO todo_list(task) VALUES ('sample');
+INSERT INTO todo_list(task) VALUES ('data');

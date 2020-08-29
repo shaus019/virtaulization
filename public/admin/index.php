775b0095c4319959eaa0d1a8f2ff4b181ccf6b3b
Sean Moir <seanmoir99@gmail.com>
2020-08-30 02:24:20 +1200

    delete working

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title> To do list app </title>
-	</head>
-    <body>
-        <div class="heading">
-        <h2>This is an app for to do list.</h2>
-        </div>
 
-        <form method = "POST" action="index.php">
-            <input type = "text" name = "task" class = "task_input">
-            <button type = "submit" class = "task_btn" name = "submit"> Add Task </button>
-        </form>
+<head>
+    <title> To do list app </title>
+</head>
+
+<body>
+    <div class="heading">
+        <h2>This is an app for to do list.</h2>
+    </div>
+
+    <form method="POST" action="index.php">
+        <input type="text" name="task" class="task_input">
+        <button type="submit" class="task_btn" name="submit"> Edit Task </button>
+    </form>
 
     <?php
     //connect to the databse
@@ -25,31 +27,49 @@
     $errors = "";
     try {
         $conn = new PDO($pdo_dsn, $db_user, $db_passwd);
-  
+
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    } catch(PDOException $e) {
+    } catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
-    //insert a task, when submit button is clicked, and show an error if nothing has entered.
-    if(isset($_POST['submit'])){
-        if(empty($_POST['task'])){
-            $errors = "you must fill in the task";
-    }else{
-        $task = $_POST['task'];
-        $conn->query("INSERT INTO todo_list (task) VALUES ('$task')");
+    // delete task
+    if (isset($_GET['del_task'])) {
+        $id = $_GET['del_task'];
+
+        $conn->query("DELETE FROM todo_list WHERE id=" . $id);
         header('location: index.php');
-}
-}
+    }
     //retrive the data(tasks) from the databse and put in a table.
     $q = $conn->query("SELECT * FROM todo_list");
-    echo "<br><table border>";
-    while($row = $q->fetch()){
-        echo "<tr><td>".$row["id"]."</td><td>".$row["task"]."</td></tr>\n";
-    }
-    echo "</table>";
-
     ?>
-	</body>
+    <table border>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th style="width: 80px;">Task</th>
+                <th style="width: 20px;">Delete</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+            // select all tasks if page is visited or refreshed
+            $q = $conn->query("SELECT * FROM todo_list");
+
+            $i = 1;
+            while ($row = $q->fetch()) { ?>
+                <tr>
+                    <td> <?php echo $i; ?> </td>
+                    <td class="task"> <?php echo $row['task']; ?> </td>
+                    <td class="delete">
+                        <a href="index.php?del_task=<?php echo $row['id'] ?>">x</a>
+                    </td>
+                </tr>
+            <?php $i++;
+            } ?>
+        </tbody>
+    </table>
+</body>
+
 </html>

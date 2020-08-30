b87b70bca32125428b75080ceb069bfec4da339c
Usman Shah <shaus019@student.otago.ac.nz>
2020-08-31 00:57:11 +1200

    Added an action for the user to delete a task.

--- public/user/index.php
+++ public/user/index.php
@@ -34,6 +34,14 @@
     }
   }
 
+   // delete task
+   if (isset($_GET['del_task'])) {
+     $id = $_GET['del_task'];
+     //send delete request and reload page
+     $conn-> exec("DELETE FROM todo_list WHERE id=" . $id);
+     header('location: index.php');
+}
+
 
 ?>
 <!DOCTYPE html>
@@ -62,6 +70,7 @@
 		<tr>
 			<th>N</th>
 			<th>Tasks</th>
+			<th style="width: 60px;">Action</th>
 		</tr>
 	</thead>
 
@@ -74,6 +83,9 @@
 			<tr>
 				<td> <?php echo $i; ?> </td>
 				<td class="task"> <?php echo $row['task']; ?> </td>
+				<td class="delete"> 
+					<a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
+				</td>
 			</tr>
 		<?php $i++; } ?>	
 	</tbody>

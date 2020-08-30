a2cb06946d9ccd89a7aefe5771a273175031786f
Usman Shah <shaus019@student.otago.ac.nz>
2020-08-31 00:00:57 +1200

    Error message is visible now, when the form is empty.

--- public/user/index.php
+++ public/user/index.php
@@ -1,26 +1,5 @@
-<!DOCTYPE html>
-
-<head>
-  <title> Todo list user portal </title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-
-<body>
-  <div class="heading">
-    <h2>Todo list</h2>
-  </div>
-
-  <form method="POST" action="index.php">
-    <input type="text" name="task" class="task_input">
-    <button type="submit" class="task_btn" name="submit"> Add Task </button>
-  </form>
-  <?php 
-  if (isset($errors)) {
-      echo "<p style=\"text-color: red;\">$errors</p>";
-  } 
-
-
-  //connect to the databse
+<?php 
+//connect to the databse
   $db_host   = '192.168.33.20';
   $db_name   = 'todo';
   $db_user   = 'webuser';
@@ -54,18 +33,40 @@
       header('location: index.php');
     }
   }
+  ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title> Todo list user portal </title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <div class="heading">
+    <h2>Todo list</h2>
+  </div>
+
+  <form method="POST" action="index.php">
+  <?php if (isset($errors)) { ?>
+  	<p><?php echo $errors; ?></p>
+  <?php } ?>
+    <input type="text" name="task" class="task_input">
+    <button type="submit" class="task_btn" name="submit"> Add Task </button>
+  </form>
+  <?php
   //retrive the data(tasks) from the databse and put in a table.
   $q = $conn->query("SELECT * FROM todo_list");
 
   //print table of all tasks
-  echo "<br><table border>";
+ echo "<br><table border>";
 
   while ($row = $q->fetch()) {
     echo "<tr><td>" . $row["id"] . "</td><td>" . $row["task"] . "</td></tr>\n";
   }
   echo "</table>";
   ?>
-
+ 
 </body>
 
 </html>
